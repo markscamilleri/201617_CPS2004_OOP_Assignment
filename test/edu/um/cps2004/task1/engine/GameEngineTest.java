e4f8542eec0ce3b6810931de407764964b90171b
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2017-01-07 11:43:55 +0100

    Added new tests including other players

--- test/edu/um/cps2004/task1/engine/GameEngineTest.java
+++ test/edu/um/cps2004/task1/engine/GameEngineTest.java
@@ -1,11 +1,18 @@
 package edu.um.cps2004.task1.engine;
 
 import edu.um.cps2004.task1.catalog.Mark;
+import edu.um.cps2004.task1.robot.Miguel;
 import edu.um.cps2004.task1.robot.TTTRobot;
+import edu.um.cps2004.task1.robot.ella;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * @author Mark Said Camilleri
@@ -33,9 +40,12 @@ public class GameEngineTest {
     TTTRobot robotO = new Robot();
     TTTRobot robotX = new Robot();
 
+    ByteArrayOutputStream out;
+
     @Before
-    public void initialize() {
-        engine = new GameEngine(robotO, robotX);
+    public void initialize(){
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
     }
 
     /**
@@ -43,7 +53,57 @@ public class GameEngineTest {
      * a test suite. This method is one specific test.
      */
     @Test
-    public void testGameBoardIsCreated() {
+    public void testGameEngineCreated(){
+        engine = new GameEngine(robotO, robotX);
         assertNotNull("The Game has not been created", engine);
     }
+
+    /**
+     * Tests if the IllegalArgumentException is thrown
+     * when the first robot is null
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testGameEngineFirstNull(){
+        engine = new GameEngine(null, robotX);
+    }
+
+    /**
+     * Tests if the IllegalArgumentException is thrown
+     * when the second robot is null
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testGameEngineSecondNull(){
+        engine = new GameEngine(robotO, null);
+    }
+
+    /**
+     * Tests if the IllegalArgumentException is thrown
+     * when the both robots are null
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testGameEngineBothNull(){
+        engine = new GameEngine(null, null);
+    }
+
+    /**
+     * Checks whether the robots are valid
+     */
+    @Test
+    public void testGameEngineCreatedWithActualRobots(){
+        engine = new GameEngine(new ella(), new Miguel());
+        assertNotNull("The Game has not been created with actual robots", engine);
+    }
+
+    /**
+     * Checks whether a winner was output
+     */
+    @Test
+    public void testGameEnginePlay(){
+        engine = new GameEngine(new ella(), new Miguel());
+        String output = new String(out.toByteArray(), StandardCharsets.UTF_8);
+        String[] outputs = output.split("\n");
+
+        assertTrue(outputs[outputs.length - 1].contains("Miguel") || outputs[outputs.length - 1].contains("Ella"))
+    }
+
 }
